643aacc19af945729a7cc9b9376a1d86a3dd42d9
Radtoli <68783009+Radtoli@users.noreply.github.com>
2021-08-02 19:00:06 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.get("/contato", (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.get("/calendario", (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.get("/calendario/*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(process.env.PORT || 3000);
+
+
